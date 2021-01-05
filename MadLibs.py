0f4a749943517eecb4074fc3a7b686bd00dd21c2
Mohamed A <76791648+mohamedA007@users.noreply.github.com>
2021-01-05 16:42:50 +0000

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,8 @@
+color = input('Enter a color: ')
+noun = input('Enter a noun: ')
+who= input('Enter a person name: ')
+
+print("Roses are " + color)
+print(noun + " are blue")
+print("I love "+ who)
+
---
+++ lists.py
@@ -0,0 +1,1 @@
+print('The {b} {t} {c}'.format(t='tall',c='company',b='blue'))
